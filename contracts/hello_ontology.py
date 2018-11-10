47621d28c237505a0aed630236ebbb378c7f506e
lucas7788 <sishsh@163.com>
2018-11-10 12:28:35 +0800

    update contract and default-config.json

--- contracts/hello_ontology.py
+++ contracts/hello_ontology.py
@@ -1,14 +1,76 @@
-from boa.interop.System.Runtime import Log
-
+from boa.interop.System.Runtime import Notify
 
 def Main(operation, args):
     if operation == 'hello':
         msg = args[0]
         return hello(msg)
-
+    if operation == 'testListNum':
+        if len(args) != 1:
+            return False
+        msgList = args[0]
+        return testListNum(msgList)
+    if operation == 'testListNum2':
+        Notify([args])
+        if len(args) != 2:
+            return False
+        msgList = args[0]
+        msg = args[1]
+        return testListNum2(msgList, msg)
+    if operation == 'testListStr':
+        if len(args) != 2:
+            return False
+        msgList = args[0]
+        msg = args[1]
+        return testListStr(msgList, msg)
+    if operation == 'testListByteArray':
+        if len(args) !=2:
+            return False
+        msgList = args[0]
+        msg = args[1]
+        return testListByteArray(msgList, msg)
+    if operation == 'testListStruct':
+        if len(args) !=2:
+            return False
+        msgList = args[0]
+        msg = args[1]
+        return testListStruct(msgList,msg)
     return False
 
-
 def hello(msg):
-    Log(msg)
-    return True
+    Notify([msg])
+    return msg
+
+def testListNum(msg):
+    Notify([msg])
+    return msg
+
+def testListNum2(msgList, msg):
+    Notify([msgList])
+    Notify([msg])
+    resList = []
+    resList.append(msgList)
+    resList.append(msg)
+    return resList
+
+def testListStr(msgList, msg):
+    Notify([msgList])
+    Notify([msg])
+    resList = []
+    resList.append(msgList)
+    resList.append(msg)
+    return resList
+def testListByteArray(msgList, msg):
+    Notify([msgList])
+    Notify([msg])
+    resList = []
+    resList.append(msgList)
+    resList.append(msg)
+    return resList
+def testListStruct(msgList, msg):
+    Notify([msg])
+    resList = []
+    for m in msgList:
+        Notify([m])
+        resList.append(m)
+    resList.append(msg)
+    return resList
