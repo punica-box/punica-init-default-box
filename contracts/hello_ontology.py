8c9f19a89d6796e33cb4e4983647b91cfbef9dee
lucas7788 <sishsh@163.com>
2018-11-14 17:32:47 +0800

    update default-config.json hello contract

--- contracts/hello_ontology.py
+++ contracts/hello_ontology.py
@@ -1,81 +1,92 @@
 from boa.interop.System.Runtime import Notify
 
 def Main(operation, args):
-    if operation == 'hello':
-        msg = args[0]
-        return hello(msg)
-    if operation == 'testListNum':
+    if operation == 'testHello':
+        if len(args) != 6:
+            return False
+        msgBool = args[0]
+        msgInt = args[1]
+        msgByteArray = args[2]
+        msgStr = args[3]
+        msgHex = args[4]
+        msgAddress = args[5]
+        return testHello(msgBool, msgInt, msgByteArray,msgStr,msgHex,msgAddress)
+    if operation == 'testNumList':
         if len(args) != 1:
             return False
-        msgList = args[0]
-        return testListNum(msgList)
-    if operation == 'testListNumStr':
+        numList = args[0]
+        return testNumList(numList)
+    if operation == 'testNumListAndStr':
         Notify([args])
         if len(args) != 2:
             return False
-        msgList = args[0]
-        msg = args[1]
-        return testListNumStr(msgList, msg)
-    if operation == 'testListStr':
+        numList = args[0]
+        msgStr = args[1]
+        return testNumListAndStr(numList, msgStr)
+    if operation == 'testStrListAndStr':
         if len(args) != 2:
             return False
-        msgList = args[0]
-        msg = args[1]
-        return testListStr(msgList, msg)
-    if operation == 'testListByteArray':
+        strList = args[0]
+        msgStr = args[1]
+        return testStrListAndStr(strList, msgStr)
+    if operation == 'testByteArrayListAndStr':
         if len(args) !=2:
             return False
         msgList = args[0]
         msg = args[1]
-        return testListByteArray(msgList, msg)
-    if operation == 'testListStruct':
+        return testByteArrayListAndStr(msgList, msg)
+    if operation == 'testStructList':
         Notify(args)
-        msgList = args[0]
-        return testListStruct(msgList)
-    if operation == 'testListStructStr':
+        structList = args[0]
+        return testStructList(structList)
+    if operation == 'testStructListAndStr':
         if len(args) !=2:
             return False
-        msgList = args[0]
-        msg = args[1]
-        return testListStruct(msgList,msg)
+        structList = args[0]
+        msgStr = args[1]
+        return testStructListAndStr(structList,msgStr)
     return False
 
-def hello(msg):
-    Notify([msg])
-    return msg
-
-def testListNum(msg):
-    Notify([msg])
-    return msg
-
-def testListNumStr(msgList, msg):
-    Notify([msgList])
-    Notify([msg])
+def testHello(msgBool, msgInt, msgByteArray,msgStr,msgHex,msgAddress):
+    Notify(["testHello",msgBool, msgInt, msgByteArray,msgStr,msgHex,msgAddress])
     resList = []
-    resList.append(msgList)
-    resList.append(msg)
+    resList.append(msgBool)
+    resList.append(msgInt)
+    resList.append(msgByteArray)
+    resList.append(msgStr)
+    resList.append(msgHex)
+    resList.append(msgAddress)
     return resList
 
-def testListStr(msgList, msg):
-    Notify([msgList])
-    Notify([msg])
+def testNumList(numList):
+    Notify(["testNumList", numList])
+    return numList
+
+def testNumListAndStr(numList, msgStr):
+    Notify(["testNumListAndStr",numList,msgStr])
     resList = []
-    resList.append(msgList)
-    resList.append(msg)
+    resList.append(numList)
+    resList.append(msgStr)
     return resList
-def testListByteArray(msgList, msg):
-    Notify([msgList])
-    Notify([msg])
+
+def testStrListAndStr(strList, msgStr):
+    Notify(["testStrListAndStr", strList, msgStr])
     resList = []
-    resList.append(msgList)
-    resList.append(msg)
+    resList.append(strList)
+    resList.append(msgStr)
     return resList
-def testListStruct(msgList):
-    Notify([msgList])
-    return msgList
-def testListStructStr(msgList, msg):
-    Notify([msgList, msg])
+def testByteArrayListAndStr(bytearrayList, msgStr):
+    Notify(["testByteArrayListAndStr", bytearrayList, msgStr])
     resList = []
-    resList.append(msgList)
-    resList.append(msg)
+    resList.append(bytearrayList)
+    resList.append(msgStr)
+    return resList
+def testStructList(structList):
+    Notify(["testStructList", structList])
+    return structList
+def testStructListAndStr(structList, msgStr):
+    Notify(["testStructListAndStr", structList, msgStr])
+    resList = []
+    resList.append(structList)
+    resList.append(msgStr)
     return resList
