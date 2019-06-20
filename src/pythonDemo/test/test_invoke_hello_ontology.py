1f7c9c0222b136d98dacad15c7fa32e32e1d0b61
dingxin <wdx7266@vip.qq.com>
2019-06-20 15:27:57 +0800

    update UT

--- src/pythonDemo/test/test_invoke_hello_ontology.py
+++ src/pythonDemo/test/test_invoke_hello_ontology.py
@@ -12,32 +12,29 @@ from ontology.wallet.wallet_manager import WalletManager
 
 from src.pythonDemo.src.invoke_hello_ontology import InvokeHelloPython
 
-ontology = Ontology()
-remote_rpc_address = 'http://polaris3.ont.io:20336'
-ontology.rpc.set_address(remote_rpc_address)
-
-root_folder = os.path.dirname(os.path.dirname(__file__))
-wallet_path = os.path.join(root_folder, 'wallet', 'wallet.json')
-contracts_folder = os.path.join(root_folder, 'contracts')
-hex_contract_address = '6fa374c57ea53680f5733d789a4f03b0ea45d82e'
 gas_limit = 20000000
 gas_price = 500
 
-wallet_manager = WalletManager()
-wallet_manager.open_wallet(wallet_path)
-password = 'password'
-acct = wallet_manager.get_account_by_b58_address('AKeDu9QW6hfAhwpvCwNNwkEQt1LkUQpBpW', password)
-hello_ontology = InvokeHelloPython(ontology, hex_contract_address)
-
 
 class TestHelloOntology(unittest.TestCase):
+    def setUp(self):
+        root_folder = os.path.dirname(os.path.dirname(__file__))
+        wallet_path = os.path.join(root_folder, 'wallet', 'wallet.json')
+        self.ontology = Ontology()
+        self.ontology.rpc.set_address('http://polaris3.ont.io:20336')
+        self.ontology.wallet_manager.open_wallet(wallet_path)
+        self.acct = self.ontology.wallet_manager.get_account_by_b58_address('AKeDu9QW6hfAhwpvCwNNwkEQt1LkUQpBpW',
+                                                                            'password')
+        self.hex_contract_address = '6fa374c57ea53680f5733d789a4f03b0ea45d82e'
+        self.hello_ontology = InvokeHelloPython(self.ontology, self.hex_contract_address)
+
     def test_name(self):
-        response = hello_ontology.name()
+        response = self.hello_ontology.name()
         self.assertEqual('name', response)
 
     def test_hello(self):
         msg = 'ontology'
-        response = hello_ontology.hello(msg)
+        response = self.hello_ontology.hello(msg)
         self.assertEqual(msg, response)
 
     def test_test_hello(self):
@@ -45,12 +42,13 @@ class TestHelloOntology(unittest.TestCase):
         int_msg = 1
         bytes_msg = b'Hello'
         str_msg = 'Hello'
-        address_msg = acct.get_address().to_bytes()
-        tx_hash = hello_ontology.test_hello(bool_msg, int_msg, bytes_msg, str_msg, address_msg, acct, gas_limit,
-                                            gas_price)
+        address_msg = self.acct.get_address().to_bytes()
+        tx_hash = self.hello_ontology.test_hello(bool_msg, int_msg, bytes_msg, str_msg, address_msg, self.acct,
+                                                 gas_limit,
+                                                 gas_price)
         time.sleep(6)
-        event = ontology.rpc.get_contract_event_by_tx_hash(tx_hash)
-        states = Event.get_states_by_contract_address(event, hex_contract_address)
+        event = self.ontology.rpc.get_contract_event_by_tx_hash(tx_hash)
+        states = Event.get_states_by_contract_address(event, self.hex_contract_address)
         states[0] = Data.to_utf8_str(states[0])
         self.assertEqual('testHello', states[0])
         states[1] = Data.to_bool(states[1])
@@ -62,14 +60,14 @@ class TestHelloOntology(unittest.TestCase):
         states[4] = Data.to_utf8_str(states[4])
         self.assertEqual(str_msg, states[4])
         states[5] = Data.to_b58_address(states[5])
-        self.assertEqual(acct.get_address_base58(), states[5])
+        self.assertEqual(self.acct.get_address_base58(), states[5])
 
     def test_test_list_and_str(self):
         list_msg = [1, 2, 3]
-        tx_hash = hello_ontology.test_list(list_msg, acct, gas_limit, gas_price)
+        tx_hash = self.hello_ontology.test_list(list_msg, self.acct, gas_limit, gas_price)
         time.sleep(6)
-        event = ontology.rpc.get_contract_event_by_tx_hash(tx_hash)
-        states = Event.get_states_by_contract_address(event, hex_contract_address)
+        event = self.ontology.rpc.get_contract_event_by_tx_hash(tx_hash)
+        states = Event.get_states_by_contract_address(event, self.hex_contract_address)
         states[0] = Data.to_utf8_str(states[0])
         self.assertEqual('testMsgList', states[0])
         states[1] = Data.to_int_list(states[1])
@@ -77,18 +75,18 @@ class TestHelloOntology(unittest.TestCase):
 
     def test_test_dict_pre_exec(self):
         dict_msg = {'key': 'value'}
-        dict_value = hello_ontology.test_dict_pre_exec(dict_msg)
+        dict_value = self.hello_ontology.test_dict_pre_exec(dict_msg)
         dict_value = Data.to_utf8_str(dict_value.get('Result', ''))
         self.assertEqual('value', dict_value)
 
     def test_test_dict(self):
         dict_msg = {'key': 'value'}
-        tx_hash = hello_ontology.test_dict(dict_msg, acct, gas_limit, gas_price)
+        tx_hash = self.hello_ontology.test_dict(dict_msg, self.acct, gas_limit, gas_price)
         self.assertEqual(64, len(tx_hash))
 
     def test_test_get_dict(self):
         key = 'key'
-        value = hello_ontology.test_get_dict(key)
+        value = self.hello_ontology.test_get_dict(key)
         value = Data.to_utf8_str(value.get('Result', ''))
         self.assertEqual('value', value)
 
@@ -99,7 +97,7 @@ class TestHelloOntology(unittest.TestCase):
         str_msg = 'Hello'
         list_msg = [1, 10, 1024, [1, 10, 1024, [1, 10, 1024]]]
         struct_list = [bool_msg, int_msg, bytes_msg, str_msg, list_msg]
-        value = hello_ontology.test_struct_list_and_str_pre_exec(struct_list, str_msg)
+        value = self.hello_ontology.test_struct_list_and_str_pre_exec(struct_list, str_msg)
         value[0][0] = Data.to_bool(value[0][0])
         self.assertEqual(bool_msg, value[0][0])
         value[0][1] = Data.to_int(value[0][1])
@@ -120,10 +118,10 @@ class TestHelloOntology(unittest.TestCase):
         str_msg = 'Hello'
         list_msg = [1, 10, 1024, [1, 10, 1024, [1, 10, 1024]]]
         struct_list = [bool_msg, int_msg, bytes_msg, str_msg, list_msg]
-        tx_hash = hello_ontology.test_struct_list_and_str(struct_list, str_msg, acct, gas_limit, gas_price)
+        tx_hash = self.hello_ontology.test_struct_list_and_str(struct_list, str_msg, self.acct, gas_limit, gas_price)
         time.sleep(6)
-        event = ontology.rpc.get_contract_event_by_tx_hash(tx_hash)
-        states = Event.get_states_by_contract_address(event, hex_contract_address)
+        event = self.ontology.rpc.get_contract_event_by_tx_hash(tx_hash)
+        states = Event.get_states_by_contract_address(event, self.hex_contract_address)
         states[0] = Data.to_utf8_str(states[0])
         states[1][0] = Data.to_bool(states[1][0])
         self.assertEqual(bool_msg, states[1][0])
@@ -144,10 +142,10 @@ class TestHelloOntology(unittest.TestCase):
         dict_value = {'key': 'value'}
         list_value = [1, 10, 1024, [1, 10, 1024, [1, 10, 1024]]]
         dict_msg = {'key': dict_value, 'key1': int_value, 'key2': str_value, 'key3': bool_value, 'key4': list_value}
-        tx_hash = hello_ontology.test_dict_in_ctx(dict_msg, acct, gas_limit, gas_price)
+        tx_hash = self.hello_ontology.test_dict_in_ctx(dict_msg, self.acct, gas_limit, gas_price)
         time.sleep(6)
-        event = ontology.rpc.get_contract_event_by_tx_hash(tx_hash)
-        states = Event.get_states_by_contract_address(event, hex_contract_address)
+        event = self.ontology.rpc.get_contract_event_by_tx_hash(tx_hash)
+        states = Event.get_states_by_contract_address(event, self.hex_contract_address)
         states[0] = Data.to_utf8_str(states[0])
         self.assertEqual('mapInfo', states[0])
         states[1] = Data.to_dict(states[1])
@@ -155,18 +153,18 @@ class TestHelloOntology(unittest.TestCase):
 
     def test_test_get_dict_in_ctx(self):
         key = 'key'
-        value = hello_ontology.test_get_dict_in_ctx(key)
+        value = self.hello_ontology.test_get_dict_in_ctx(key)
         value = Data.to_utf8_str(value.get('Result', ''))
         self.assertEqual('value', value)
 
     def test_test_transfer_multi(self):
-        bytes_address = acct.get_address().to_bytes()
+        bytes_address = self.acct.get_address().to_bytes()
         transfer1 = [bytes_address, bytes_address, 1]
         transfer2 = [bytes_address, bytes_address, 2]
         transfer_list = [transfer1, transfer2]
-        tx_hash = hello_ontology.test_transfer_multi(transfer_list, acct, gas_limit, gas_price)
+        tx_hash = self.hello_ontology.test_transfer_multi(transfer_list, self.acct, gas_limit, gas_price)
         time.sleep(6)
-        event = ontology.rpc.get_contract_event_by_tx_hash(tx_hash)
+        event = self.ontology.rpc.get_contract_event_by_tx_hash(tx_hash)
         self.assertEqual(1, event['State'])
 
 
